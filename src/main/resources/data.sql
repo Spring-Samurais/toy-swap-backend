f4802e07f00aa92498207fbb784887f5cab57473
Lonrot <angelschmal.1a@gmail.com>
2024-06-26 22:05:46 +0100

    Feat: Inserter post

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,11 @@
 INSERT INTO MEMBER (name, nickname, location) VALUES ('John Doe', 'johndoe', 'New York');
 INSERT INTO MEMBER (name, nickname, location) VALUES  ('Willy Smith', 'wsmithy', 'Black Pool');
+
+--------------------------------------------------------------------------------------------------------------------
+
+
+INSERT INTO Post (photo, poster_id, date_posted, category, description, condition, status)
+VALUES (NULL, 1, CURRENT_TIMESTAMP, 'ACTION_FIGURES', 'John\''s action figure', 'New', 'AVAILABLE');
+
+INSERT INTO Post (photo, poster_id, date_posted, category, description, condition, status)
+VALUES ('', 2, CURRENT_TIMESTAMP, 'DOLLS', 'Jane''s doll', 'Used', 'AVAILABLE');
