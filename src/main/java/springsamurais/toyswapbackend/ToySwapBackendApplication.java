83ff553319ebc8c363928ec0dc00bc2a8fd77756
Lonrot <angelschmal.1a@gmail.com>
2024-06-26 14:48:51 +0100

    Initial commit

---
+++ src/main/java/springsamurais/toyswapbackend/ToySwapBackendApplication.java
@@ -0,0 +1,13 @@
+package springsamurais.toyswapbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToySwapBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToySwapBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springsamurais/toyswapbackend/ToySwapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package springsamurais.toyswapbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToySwapBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
