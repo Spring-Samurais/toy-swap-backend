79ca22ae9bfd987843cbbdefca0eb5f495651e4e
Lonrot <angelschmal.1a@gmail.com>
2024-06-26 20:46:41 +0100

    Feat: Added users

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO MEMBER (name, nickname, location) VALUES ('John Doe', 'johndoe', 'New York');
+INSERT INTO MEMBER (name, nickname, location) VALUES  ('Willy Smith', 'wsmithy', 'Black Pool');
