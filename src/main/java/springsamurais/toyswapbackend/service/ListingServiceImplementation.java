335523ef4002ef904790d143c6168ab08f740cf1
Lonrot <angelschmal.1a@gmail.com>
2024-06-27 23:48:53 +0100

    Feat(service): Logic to find Listing by ID added. Test passed.

--- src/main/java/springsamurais/toyswapbackend/service/ListingServiceImplementation.java
+++ src/main/java/springsamurais/toyswapbackend/service/ListingServiceImplementation.java
@@ -3,7 +3,8 @@ package springsamurais.toyswapbackend.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import springsamurais.toyswapbackend.model.Listing;
+import springsamurais.toyswapbackend.exception.ListingNotFoundException;
+import springsamurais.toyswapbackend.model.*;
 import springsamurais.toyswapbackend.repository.ListingRepository;
 
 import java.util.ArrayList;
@@ -22,6 +23,12 @@ public class ListingServiceImplementation implements ListingService {
         List<Listing> listingsListResult = new ArrayList<>();
         listingRepository.findAll().forEach(listingsListResult::add);
         return listingsListResult;
+    }
+
+    @Override
+    public Listing getListingById(Long id) {
+        return listingRepository.findById(id)
+                .orElseThrow(() -> new ListingNotFoundException("Listing with ID " + id + " not found"));
     }
 
 }
