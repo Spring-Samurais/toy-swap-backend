56cc862df4860828f1010838ed9b5fe08acbea33
Lonrot <angelschmal.1a@gmail.com>
2024-06-27 23:07:29 +0100

    Fix: Weird issue with the initializer pointing at some other places, Found that with @Transactional I can define the scope without relaying in spring auto.

--- src/main/java/springsamurais/toyswapbackend/service/ListingServiceImplementation.java
+++ src/main/java/springsamurais/toyswapbackend/service/ListingServiceImplementation.java
@@ -2,6 +2,7 @@ package springsamurais.toyswapbackend.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import springsamurais.toyswapbackend.model.Listing;
 import springsamurais.toyswapbackend.repository.ListingRepository;
 
@@ -10,6 +11,7 @@ import java.util.List;
 
 
 @Service
+@Transactional // This annotation here point at this class for any transaction with the DB, please refer to Docs for more info :)  -Angel
 public class ListingServiceImplementation implements ListingService {
 
     @Autowired
