e71558f786bea8120bdba66c9f408db779b6fa01
Bilal Musa <musabilal93@gmail.com>
2024-06-28 17:10:13 +0100

    exceptionHandling added

---
+++ src/main/java/springsamurais/toyswapbackend/exception/ListingFailedToSaveException.java
@@ -0,0 +1,9 @@
+package springsamurais.toyswapbackend.exception;
+
+public class ListingFailedToSaveException extends RuntimeException{
+
+    public ListingFailedToSaveException(String message){
+        super(message);
+    }
+
+}
