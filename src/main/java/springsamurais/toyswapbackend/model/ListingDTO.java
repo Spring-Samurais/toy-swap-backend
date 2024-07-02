96c1aef165b9a106672da35efdef924331e79fb2
Lonrot <angelschmal.1a@gmail.com>
2024-07-02 15:56:43 +0100

    Feat(DTO): DTO now can take an array of multiparfiles and safe them to the Image table and append them to the Listing

--- src/main/java/springsamurais/toyswapbackend/model/ListingDTO.java
+++ src/main/java/springsamurais/toyswapbackend/model/ListingDTO.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
+import springsamurais.toyswapbackend.service.imgurapi.service.ImgurService;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
 @Data
@@ -21,20 +23,36 @@ public class ListingDTO {
     private String description;
     private String condition;
     private String statusListing;
+    private List<MultipartFile> imageFiles; // Add this field to handle multiple images
 
 
-    public Listing toEntity(Member member, MultipartFile imageData) throws IOException {
+
+    public Listing toEntity(Member member, ImgurService imgurService) throws IOException {
         Listing listing = new Listing();
 
         listing.setTitle(this.title);
         listing.setMember(member);
         listing.setDatePosted(LocalDateTime.now());
-        listing.setPhoto(imageData.getBytes());
         listing.setCategory(Category.valueOf(this.category));
         listing.setDescription(this.description);
         listing.setCondition(ItemCondition.valueOf(this.condition));
         listing.setStatusListing(Status.valueOf(this.statusListing));
         listing.setComments(null);
+
+
+        List<Image> imagesList = new ArrayList<>();
+        //I can probably use a stream - Angel
+        for (MultipartFile file : imageFiles) {
+            String base64Image = Base64.getEncoder().encodeToString(file.getBytes());
+            String url = imgurService.uploadImage(base64Image);
+            Image image = new Image();
+            image.setImageName(file.getOriginalFilename());
+            image.setUrl(url);
+            image.setListing(listing);
+            imagesList.add(image);
+        }
+        listing.setImages(imagesList);
+
         return listing;
     }
 }
