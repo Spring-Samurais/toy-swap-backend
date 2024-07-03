3de395330886b757eaa868672986242ed160f5eb
Lonrot <angelschmal.1a@gmail.com>
2024-07-03 21:01:26 +0100

    Feat: Added service layer to upload images to S3 bucket and the controller needs to be finish

---
+++ src/main/java/springsamurais/toyswapbackend/service/s3service/controller/S3Controller.java
@@ -0,0 +1,9 @@
+package springsamurais.toyswapbackend.service.s3service.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/images")
+public class S3Controller {}
